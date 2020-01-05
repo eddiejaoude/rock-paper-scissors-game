feb0ff669a49bfbb4a06623d2a8ea3de3225de99
Eddie Jaoude <eddie@jaoudestudios.com>
2020-01-05 18:57:38 +0000

    YouTube example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rock-paper-scissors-game';
+
+  public computerResult: string;
+  public result: string;
+  public results = { computer: 0, player: 0 };
+
+  private userResult: string;
+
+  public play(action: string): void {
+    console.log('USER: ', action);
+    this.userResult = action;
+    this.computer();
+    this.calculateWinner();
+  }
+
+  private computer(): void {
+    const randomNumber = Math.floor(Math.random() * 3);
+    const options: string[] = [ 'rock', 'paper', 'scissors' ];
+
+    console.log('COMPUTER: ', options[randomNumber]);
+    this.computerResult = options[randomNumber];
+  }
+
+  private calculateWinner(): void {
+    if (this.userResult === this.computerResult) {
+      this.result = 'There was a tie / draw';
+    }
+
+    if (this.userResult === 'rock' && this.computerResult === 'paper') {
+      this.results.computer++;
+      this.result = 'Computer wins';
+    }
+    if (this.userResult === 'rock' && this.computerResult === 'scissors') {
+      this.results.player++;
+      this.result = 'You win';
+    }
+    if (this.userResult === 'paper' && this.computerResult === 'rock') {
+      this.results.player++;
+      this.result = 'You win';
+    }
+    if (this.userResult === 'paper' && this.computerResult === 'scissors') {
+      this.results.computer++;
+      this.result = ' Computer wins';
+    }
+    if (this.userResult === 'scissors' && this.computerResult === 'rock') {
+      this.results.computer++;
+      this.result = ' Computer wins';
+    }
+    if (this.userResult === 'scissors' && this.computerResult === 'paper') {
+      this.results.player++;
+      this.result = ' You win';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';4
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
